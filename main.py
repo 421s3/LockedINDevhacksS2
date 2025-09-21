ec81409942bce6976404af50e82f4f7f29487c75
Xayden Kamei <xayden22@gmail.com>
2025-09-20 17:20:52 -0700

    input changes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import webScrapeASU.scraperHelper as sh
 import asyncio
 
-print("hello world")
 
 
+classType = input("Please enter 3 digit class type")
+classNumber = input("Please enter 3 digit class number")
+classTerm = input("Please enter class term, in format 2XXY with XX being the last 2 digits of the year and Y being the term (1 is fall, 4 is summer, and 7 is spring)")
 async def schedule():
-    x = await sh.get_schedule("CSE",240,2251)
-    print(x)
+    x = await sh.get_schedule(classType,classNumber,classTerm)
 asyncio.run(schedule())
